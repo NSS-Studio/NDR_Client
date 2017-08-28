acf29db7b284455e8d1580fcf2c4acf902464d9e
cxhwd <cxhwd@163.com>
2017-08-28 01:41:11 +0000

    Update common.h

--- client/common.h
+++ client/common.h
@@ -12,12 +12,12 @@
 #include <QFontDatabase>
 #include <QtCore/QtGlobal>
 
-#define NDR_INNER true
+#define NDR_TESTING true
 /** server address*/
 #define NDR_SERVER_INNER "172.24.5.130"
 #define NDR_SERVER_OUTER "172.24.10.13"
 
-#if (NDR_INNER == true)
+#if (NDR_TESTING == true)
 #define NDR_SERVER NDR_SERVER_INNER
 #else
 #define NDR_SERVER NDR_SERVER_OUTER
